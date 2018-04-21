6629782d2641a713dd914183e2609793110aae42
Tom <tmbdev@gmail.com>
2018-04-21 15:30:23 -0700

    added model download, recognizer

--- setup.py
+++ setup.py
@@ -16,9 +16,16 @@ from distutils.core import setup  # , Extension, Command
 assert sys.version_info[0] == 2 and sys.version_info[1] >= 7,\
     "requires Python version 2.7 or later, but not Python 3.x"
 
+remote = "https://storage.googleapis.com/tmb-models/"
+models = ["line2-000003330-004377.pt"]
+for model in models:
+    if not os.path.exists(model):
+        print("downloading", remote+model)
+        urllib.URLopener().retrieve(remote+model, model)
 
 scripts = """
 ocroline-train
+ocroline-pred
 """.split()
 
 setup(
@@ -28,4 +35,5 @@ setup(
     description="Text line recognition.",
     packages=["ocroline"],
     scripts=scripts,
+    data_files = [("share/ocroline", models)]
 )
